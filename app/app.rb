2462aaa5af616fef05baaa3d7dc8ff71e31b980a
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-19 16:27:10 +0100

    add multiple environments

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,8 @@
+ENV["RACK_ENV"] ||=  "development"
 require 'sinatra/base'
 require_relative 'models/link'
 
 class Bookmark < Sinatra::Base
-
-enable :sessions
 
 get '/links' do
   @links = Link.all
