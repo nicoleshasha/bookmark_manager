6fba833edf7b5a8280f1f2ae6b08a5e81750e266
Nicole <nicoleshasha@hotmail.co.uk>
2016-10-21 15:00:45 +0100

    Change datamapper setup

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'dm-migrations'
+require_relative 'data_mapper_setup'
 
 class Link
 
@@ -13,7 +14,7 @@ class Link
   has n, :tags, :through => Resource
 end
 
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
+#
+# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+ DataMapper.finalize
+ DataMapper.auto_upgrade!
